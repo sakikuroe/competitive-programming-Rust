876a832bb3ba83f4369a7c5aa90dc74ebad85812
sakikuroe <sakikuroe96@gmail.com>
2021-11-06 14:02:23 +0900

    Change the implementation for my practice

--- AtCoder/abc225/src/bin/b.rs
+++ AtCoder/abc225/src/bin/b.rs
@@ -1,40 +1,24 @@
 use proconio::input;
-
-#[derive(Debug)]
-struct Graph {
-        edges: Vec<Vec<usize>>,
-        size: usize,
-}
-impl Graph {
-        fn new(size: usize) -> Self {
-                Graph {
-                        edges: vec![vec![]; size],
-                        size,
-                }
-        }
-        fn add_edge(&mut self, src: usize, dst: usize) {
-                self.edges[src].push(dst);
-        }
-}
+use proconio::marker::Usize1;
 
 fn main() {
         // Input
         input! {
             n: usize,
-            ab_list: [(usize, usize); n-1],
+            ab_list: [(Usize1, Usize1); n - 1],
         }
 
         // Initialize
-        let mut g = Graph::new(n);
+        let mut g: Vec<Vec<usize>> = vec![vec![]; n];
         for (a, b) in ab_list.clone() {
-                g.add_edge(a - 1, b - 1);
-                g.add_edge(b - 1, a - 1);
+                g[a].push(b);
+                g[b].push(a);
         }
 
         // Solve
         let mut ans = false;
         for i in 0..n {
-                if g.edges[i].len() == n - 1 {
+                if g[i].len() == n - 1 {
                         ans = true;
                 }
         }
