070452cd1b78b66c4d0f9424c2f402b6e833638b
sakikuroe <sakikuroe96@gmail.com>
2021-12-17 18:20:42 +0900

    Change the implementation from scratch for my practice

--- AtCoder/abc106/src/bin/b.rs
+++ AtCoder/abc106/src/bin/b.rs
@@ -1,32 +1,25 @@
 use proconio::input;
 
 fn main() {
-        // Input
-        input! {
-            n: usize,
+    // Input
+    input! {
+        n: usize,
+    }
+
+    // Solve
+    let mut ans = 0;
+    for i in 1..=n {
+        let mut cnt = 0;
+        for j in 1..=i {
+            if i % j == 0 {
+                cnt += 1;
+            }
         }
+        if cnt == 8 && i % 2 == 1 {
+            ans += 1;
+        }
+    }
 
-        // Solve
-        let ans = {
-                let mut res = 0;
-                for i in 1..=n {
-                        if i % 2 == 0 {
-                                continue;
-                        }
-                        let mut cnt = 0;
-                        for j in 1..=i {
-                                if i % j == 0 {
-                                        cnt += 1;
-                                }
-                        }
-                        if cnt == 8 {
-                                res += 1;
-                        }
-                }
-
-                res
-        };
-
-        // Output
-        println!("{}", ans);
+    // Output
+    println!("{}", ans);
 }
