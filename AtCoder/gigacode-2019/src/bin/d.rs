8fc460482860dec2b2bc35f82c38d8e44b5a0c08
sakikuroe <sakikuroe96@gmail.com>
2021-12-27 04:09:28 +0900

    Change the implementation for my practice

--- AtCoder/gigacode-2019/src/bin/d.rs
+++ AtCoder/gigacode-2019/src/bin/d.rs
@@ -1,32 +1,20 @@
 use proconio::input;
-use std::cmp;
 
 fn main() {
     // Input
     input! {
         h: usize, w: usize, k: usize, v: usize,
-        mut a_mat: [[usize; w]; h],
+        a: [[usize; w]; h],
     }
 
-    // Initialize
-    let a_mat = {
-        for i in 0..h {
-            for j in 0..w {
-                a_mat[i][j] += k;
-            }
-        }
-        a_mat
-    };
-
     // Solve
-    let sum_mat: Vec<Vec<usize>> = {
-        let mut res: Vec<Vec<usize>> = vec![vec![0; w + 1]; h + 1];
+    let s = {
+        let mut res = vec![vec![0; w + 1]; h + 1];
         for i in 0..h {
             for j in 0..w {
-                res[i + 1][j + 1] = a_mat[i][j];
+                res[i + 1][j + 1] = a[i][j] + k;
             }
         }
-
         for i in 0..h {
             for j in 0..w {
                 res[i + 1][j + 1] += res[i][j + 1] + res[i + 1][j] - res[i][j];
@@ -35,13 +23,14 @@ fn main() {
         res
     };
 
-    let mut ans: usize = 0;
-    for i in 0..h {
-        for j in 0..w {
-            for k in i + 1..=h {
-                for l in j + 1..=w {
-                    if sum_mat[k][l] + sum_mat[i][j] - sum_mat[k][j] - sum_mat[i][l] <= v {
-                        ans = cmp::max(ans, (k - i) * (l - j));
+    let mut ans = 0;
+    for i in 0..=h {
+        for j in 0..=w {
+            for k in 0..i {
+                for l in 0..j {
+                    let cost = s[i][j] + s[k][l] - s[i][l] - s[k][j];
+                    if cost <= v {
+                        ans = std::cmp::max(ans, (i - k) * (j - l));
                     }
                 }
             }
