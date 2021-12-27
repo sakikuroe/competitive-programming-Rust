f3a606e95793b93169a8ff366e4e891aa124f5e1
sakikuroe <sakikuroe96@gmail.com>
2021-12-28 00:14:53 +0900

    Change the implementation for my practice

--- AtCoder/abc084/src/bin/d.rs
+++ AtCoder/abc084/src/bin/d.rs
@@ -1,71 +1,45 @@
 use proconio::input;
 
-trait Prime {
-    fn is_prime(self) -> bool;
-}
-
-impl Prime for usize {
-    fn is_prime(self) -> bool {
-        if self == 0 || self == 1 {
-            false
-        } else {
-            for i in 2..=(self as f64).sqrt() as usize {
-                if self % i == 0 {
-                    return false;
-                }
-            }
-            true
+fn is_prime(n: usize) -> bool {
+    if n == 0 || n == 1 {
+        return false;
+    }
+    for i in 2..=(n as f64).powf(0.5) as usize {
+        if n % i == 0 {
+            return false;
         }
     }
-}
-
-fn solve(lr_list: &Vec<(usize, usize)>) -> Vec<usize> {
-    let mut ans_list: Vec<usize> = vec![];
-
-    // sum_list[i] := i以下にある2017に似た数
-    let sum_list: Vec<usize> = {
-
-        // like_2017_list[i] = iが2017に似た数かどうか
-        let like_2017_list: Vec<bool> = (0..100001).map(|n| n.is_prime() && ((n + 1) / 2).is_prime()).collect();
-
-        // 累積和を作る
-        let mut sum_list: Vec<usize> = vec![0; 100001];
-        for i in 1..100001 {
-            if like_2017_list[i] {
-                sum_list[i] = sum_list[i - 1] + 1;
-            } else {
-                sum_list[i] = sum_list[i - 1];
-            }
-        }
-
-        sum_list
-    };
-
-
-    // sum_listでの累積和でクエリに対する答えを高速に計算
-    for &(l, r) in lr_list {
-        ans_list.push(sum_list[r] - sum_list[l - 1]);
-    }
-
-    ans_list
-}
-
-fn output(ans_list: &Vec<usize>) {
-    for ans in ans_list {
-        println!("{}", ans);
-    }
+    return true;
 }
 
 fn main() {
     // Input
     input! {
         q: usize,
-        lr_list: [(usize, usize); q],
+        lr: [(usize, usize); q],
     }
 
     // Solve
-    let ans_list: Vec<usize> = solve(&lr_list);
+    let s = {
+        let mut res = vec![0; 100001];
+        for i in 1..=100000 {
+            if i % 2 == 1 && is_prime(i) && is_prime((i + 1) / 2) {
+                res[i] += 1;
+            }
+        }
+        for i in 0..100000 {
+            res[i + 1] += res[i];
+        }
+        res
+    };
+
+    let mut ans = vec![];
+    for (l, r) in lr {
+        ans.push(s[r] - s[l - 1]);
+    }
 
     // Output
-    output(&ans_list);
+    for ans in ans {
+        println!("{}", ans);
+    }
 }
